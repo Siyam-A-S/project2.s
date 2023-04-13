ba15d108fe19170b64f6d8100d041837506ee49e
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-04-13 11:00:37 -0400

    Update project2.s
    Calculate my range

--- project2.s
+++ project2.s
@@ -2,3 +2,10 @@
 	userInput: .space 1001		#to store user input (1000 characters maximum)
 	invalid: .asciiz "Invalid input"	#label to store invalid input message
 .text
+
+main:	
+	# calculate N and M
+	# My Howard ID: 03043178
+	# N = 26 + (3043178 % 11) = 32
+	# M = 32-10 = 22
+	# Range: 0-9, a-v, A-V
